579535060ded7d814654495493369a89332613e1
swo <swo@mit.edu>
2014-03-20 17:11:51 -0400

    rubified combine

--- combine.rb
+++ combine.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-# This program combines two files column-wise.
+# This program combines two files column-wise. The length of the output is equal to the length
+# of the first file.
 
 require 'optparse'
 
@@ -8,22 +9,19 @@ options = {:record_separator => "\t"}
 OptionParser.new do |opts|
     opts.banner = "Usage: combine.rb FILE1 FILE2 [options]"
 
-    opts.on("-F", "--separator [SEPARATOR]", "Specify record separator (default tab)") { |rs|
+    opts.on("-F", "--separator [SEPARATOR]", "Specify record separator (default tab)") do |rs|
         options[:record_separator] = rs
-    }
+    end
 end.parse!
 
 fn1, fn2 = ARGV
 
-File.open(fn1, 'r') { |f1|
-    File.open(fn2, 'r') { |f2|
-        e1 = f1.to_enum
-        e2 = f2.to_enum
-
-        loop do
-            l1 = e1.next.chomp
-            l2 = e2.next.chomp
-            puts [l1, l2].join(options[:record_separator])
-        end
-    }
-}
+File.open(fn1, 'r') do |f1|
+  File.open(fn2, 'r') do |f2|
+    f1.zip(f2) do |l1, l2|
+      # if the second file has ended, just put in a blank instead
+      l2 ||= ''
+      puts "#{l1.chomp}#{options[:record_separator]}#{l2.chomp}"
+    end
+  end
+end
