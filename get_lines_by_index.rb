a57e15991fb00055483c300cb04f71fa0a5f854e
swo <swo@mit.edu>
2014-04-17 18:37:45 -0400

    get lines by index

---
+++ get_lines_by_index.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+
+usage = <<END
+Usage: get_lines_by_index.rb index_file target_file [options]
+Finds lines in target that have a first field that matches lines from the index file
+END
+
+options = {:record_separator => "\t", :offset => 0}
+OptionParser.new do |opts|
+    opts.banner = usage
+
+    opts.on("-F", "--separator [SEPARATOR]", "Specify record separator (default tab)") { |rs| options[:record_separator] = rs }
+end.parse!
+
+raise RuntimeError, 'script takes two arguments' unless ARGV.length == 2
+
+index_fn = ARGV.shift
+
+indices = File.open(index_fn).readlines.map(&:chomp)
+
+ARGF.each do |line|
+  puts line if indices.include? line.split(options[:record_separator]).first
+end
