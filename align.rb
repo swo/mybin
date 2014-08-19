a89b835d9062c4596853c37b3002f8457dbc3b54
swo <swo@mit.edu>
2014-08-19 11:52:29 -0700

    align.rb: align two strings

---
+++ align.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+#
+# author: scott w olesen <swo@mit.edu>
+#
+# align any two strings. naively scan one string against the other, looking for identical
+# characters to make the score.
+
+require 'optparse'
+
+class Aligner
+  def initialize(a, b)
+    @a = a
+    @b = b
+
+    @best_offset = ((-@b.length + 1)..(@a.length - 1)).max_by { |offset| score(*alignment(offset)) }
+    @best_score = score(*alignment(@best_offset))
+  end
+
+  def score(a, b)
+    a.each_char.zip(b.each_char).inject(0) { |sum, chars| chars[0] == chars[1] ? sum + 1 : sum }
+  end
+
+  def alignment(offset)
+    if offset == 0
+      x = @a
+      y = @b
+    elsif offset < 0
+      x = '-'*(-offset) + @a
+      d = x.length - @b.length
+      if d > 0
+        y = @b + '-'*d
+      else
+        x = x + '-'*(-d)
+        y = @b
+      end
+    elsif offset > 0
+      y = '-'*offset + @b
+      d = y.length - @a.length
+      if d > 0
+        x = @a + '-'*d
+      else
+        x = @a
+        y = y + '-'*(-d)
+      end
+    end
+
+    [x, y]
+  end
+
+  def alignment_symbol(a, b)
+    if a == b
+      '*'
+    else
+      ' '
+    end
+  end
+
+  def alignment_visual(a, b)
+    a.each_char.zip(b.each_char).collect { |c, d| alignment_symbol(c, d) }.join('')
+  end
+
+  def show_alignment(offset)
+    x, y = alignment(offset)
+    puts x
+    puts y
+    puts alignment_visual(x, y)
+  end
+
+  def show_best_alignment
+    show_alignment(@best_offset)
+  end
+
+  def show_stats
+    puts
+    puts "a length: #{@a.length}"
+    puts "b length: #{@b.length}"
+    puts "score: #{@best_score}"
+  end
+end
+
+options = {:stats => false}
+OptionParser.new do |opts|
+  opts.banner = 'usage: align.rb A B'
+
+  opts.on('-s', '--stats', 'show alignment stats?') { |x| options[:stats] = x }
+end.parse!
+
+a = ARGV.shift
+b = ARGV.shift
+
+aligner = Aligner.new(a, b)
+aligner.show_best_alignment
+aligner.show_stats if options[:stats]
