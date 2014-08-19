697d7e3b023a4e6dfe517ae3dd9175db77fe28e9
swo <swo@mit.edu>
2014-08-19 10:11:28 -0700

    reads to relative: now using pysurvey

--- reads_to_relative_abundances.py
+++ reads_to_relative_abundances.py
@@ -1,15 +1,31 @@
 #!/usr/bin/env python
 
-import survey.process as sp, survey.output as so
-import numpy as np
-import fileinput
+'''
+reads_to_relative_abundances.py
 
-sample_ids, otu_ids, table, metadata = sp.parse_otu_table(fileinput.input(), count_map_f=float)
+author: scott w olesen (swo@mit.edu)
 
-# convert all the columns to relative abundances
-n_cols = table.shape[1]
-for i in range(n_cols):
-    table[:, i] /= np.sum(table[:, i])
+goes from counts to relative abundances
+'''
 
-# write out the table
-so.write_otu_table(sample_ids, otu_ids, table, metadata)
+import pysurvey as ps, numpy as np
+import argparse
+
+if __name__ == '__main__':
+	p = argparse.ArgumentParser(description='counts to relative abundances')
+	p.add_argument('table', help='input otu table')
+	p.add_argument('out', help='output otu table')
+	p.add_argument('-p', '--pseudo', type=int, help='add pseudocounts?')
+	args = p.parse_args()
+
+	# read in table
+	table = ps.read_txt(args.table, verbose=False)
+
+	# normalize
+	if args.pseudo:
+		table = ps.to_fractions(table, method='pseudo', p_counts=arg.pseudo)
+	else:
+		table = ps.to_fractions(table, method='normalize')
+
+	# write the output table
+	ps.write_txt(table, args.out)
