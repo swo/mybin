be8c86f16a93dbf6ba4bf0a455fe435902927afc
swo <swo@mit.edu>
2014-09-03 09:35:49 -0400

    added line numbers option

--- get_lines.rb
+++ get_lines.rb
@@ -7,6 +7,7 @@ require 'arginine'
 params = Arginine::parse do |a|
   a.desc "get lines according to list"
   a.opt "offset", :short => "o", :default => 0, :cast => :to_i, :desc => "like, how many header lines?"
+  a.flag "numbers", :desc => "show line numbers?"
   a.arg "lines", :cast => lambda { |x| x.split(",").map(&:to_i) }
 end
 
@@ -18,7 +19,13 @@ ARGF.each do |line|
 
   # print out the targets if we have them in order
   while lines.key? targets.first
-    puts lines.delete(targets.shift)
+    if params["numbers"]
+      n = targets.first - params["offset"]
+      out = "#{n}:#{lines.delete(targets.shift)}"
+    else
+      out = lines.delete(targets.shift)
+    end
+    puts out
   end
 
   # stop looping if we are done
