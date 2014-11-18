dc548d2a27fdf7b276ffc5f32a96a7600ca78b15
swo <swo@mit.edu>
2014-11-18 16:50:23 -0500

    clustal: improved options

--- clustal.rb
+++ clustal.rb
@@ -3,14 +3,27 @@
 # author: scott w olesen <swo@mit.edu>
 
 require 'arginine'
+require 'tempfile'
 
 params = Arginine::parse do |a|
   desc "align sequences with clustal"
   opt :size, :desc => "fraction of screen width", :cast => :to_f, :default => 0.80
-  arg :fasta
+  flag :align_only, :desc => "show only alignment?"
+  argf "fasta"
 end
 
-raise RuntimeError unless params[:size].between?(0.0, 1.0)
+raise RuntimeError unless params[:size] > 0.0
+
+if ARGV.length == 1
+  # just run clustalo on this one file
+  params[:fasta] = ARGV.first
+else
+  # concatenate the ARGF into a tmp file
+  params[:file] = Tempfile.new("clustalo")
+  params[:file].write(ARGF.read)
+  params[:file].close
+  params[:fasta] = params[:file].path
+end
 
 # get the width of the terminal and the number of output columns
 n_cols = `tput cols`
@@ -23,4 +36,21 @@ while lines.first.empty? or lines.first.match("CLUSTAL")
   lines.shift 
 end
 
-puts lines.join("\n")
+# if showing only the alignment
+if params[:align_only]
+  # get the chunks that are separated by blank lines
+  align_groups = lines.chunk { |line| line != "" || nil }.map(&:last)
+
+  # figure out how many characters to chop from the start of each line
+  # search backward to find how many non-whitespace chars to keep
+  s = align_groups.first.first
+  start_i = s.length - s.reverse.match(/\S*/)[0].length
+
+  aligns = align_groups.map { |g| g.last[start_i..-1] }
+  puts aligns.join("\n")
+else
+  puts lines.join("\n")
+end
+
+# clean up the temporary file
+params[:file].unlink if params.key? :file
