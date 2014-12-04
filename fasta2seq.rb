c0f8b5c4752edf94f49b6ab29031af91776208c2
swo <swo@mit.edu>
2014-12-04 16:38:15 -0500

    fasta2seq: support for reading file or table

--- fasta2seq.rb
+++ fasta2seq.rb
@@ -7,13 +7,20 @@ require 'bio'
 
 params = Arginine::parse do |a|
   desc "get the fasta entry with these header(s)"
-  arg :headers
+  arg :headers, :desc => "comma-separated list or filename"
   opt :separator, :short => "F", :default => ","
+  flag :table, :desc => "does the header file have a header line?"
   argf "fasta"
 end
 
-params[:headers] = params[:headers].split(params[:separator])
+headers = []
+if File.exists? params[:headers]
+  headers = open(params[:headers]).each_line.collect { |line| line.split.first }
+  headers.shift if params[:table]
+else
+  headers = params[:headers].split(params[:separator])
+end
 
 Bio::FlatFile.auto(ARGF).each_entry do |e|
-  puts e.entry if params[:headers].include? e.definition
+  puts e.entry if headers.include? e.definition
 end
