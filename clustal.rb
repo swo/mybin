c43c03d0fe095a6e264334b615ded57f1ad9868f
swo <swo@mit.edu>
2015-01-18 23:57:19 -0500

    clustal: cleaned up, nicer align only

--- clustal.rb
+++ clustal.rb
@@ -7,12 +7,9 @@ require 'tempfile'
 
 params = Arginine::parse do |a|
   desc "align sequences with clustal"
-  opt :size, :desc => "fraction of screen width (0.0 to 1.0)", :cast => :to_f, :default => 1.0
   flag :align_only, :desc => "show only alignment?"
   argf "fasta"
 end
-
-raise RuntimeError unless params[:size] > 0.0
 
 if ARGV.length == 1
   # just run clustalo on this one file
@@ -28,11 +25,11 @@ end
 # find the length of the longest name, or 27 (whichever is smallest)
 long_name = `grep ">" #{params[:fasta]}`.split("\n").map(&:chomp).map(&:length).max
 long_name = [long_name, 27].min
+name_block_length = long_name + 6
 
 # get the width of the terminal and the number of output columns
 # for proper spacing, subtract the length of the longest name and spaces (6)
-n_cols = `tput cols`
-out_cols = (n_cols.to_f * params[:size]).floor - long_name - 6
+out_cols = `tput cols`.to_i - name_block_length
 
 # get results from clustalo. drop the first banner line and empty lines.
 lines = `clustalo --outfmt=clu --wrap=#{out_cols} -i #{params[:fasta]}`.split("\n")
@@ -46,13 +43,9 @@ if params[:align_only]
   # get the chunks that are separated by blank lines
   align_groups = lines.chunk { |line| line != "" || nil }.map(&:last)
 
-  # figure out how many characters to chop from the start of each line
-  # search backward to find how many non-whitespace chars to keep
-  s = align_groups.first.first
-  start_i = s.length - s.reverse.match(/\S*/)[0].length
-
-  aligns = align_groups.map { |g| g.last[start_i..-1] }
-  puts aligns.join("\n")
+  # chop off the name block, then tie them together
+  alignment = align_groups.map { |g| g.last[(name_block_length - 1)..-1] }.join("")
+  puts "~" + alignment + "~"
 else
   puts lines.join("\n")
 end
