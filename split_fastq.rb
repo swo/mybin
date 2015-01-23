4da3012b6a028c95f1ee4a461a87ea4310e9ad61
swo <swo@mit.edu>
2015-01-23 13:22:36 -0500

    split fastq: use better split now

--- split_fastq.rb
+++ split_fastq.rb
@@ -3,6 +3,7 @@
 # author: scott w olesen <swo@mit.edu>
 
 require 'arginine'
+require 'fileutils'
 
 params = Arginine::parse do
   desc "split a fastq into n parts"
@@ -13,32 +14,17 @@ end
 # figure out if this is a fastq by looking at the first character
 raise "file not a fastq" unless open(params[:fastq], &:readline)[0] == "@"
 
-# get the number of entries
-ne = `wc -l #{params[:fastq]}`.split.first.to_i / 4
-base_epc = ne / params[:n]
-extra_entries = ne % params[:n]
+# get the number of entries and lines per entry
+nl = `wc -l #{params[:fastq]}`.split.first.to_i
+raise "incomplete entries" unless nl % 4 == 0
+lpc = ((nl / 4).to_f / params[:n]).ceil * 4
 
-# number of digits to put after the output filenames
-nd = params[:n] / 10 + 1
+# number of chars to put after the output filenames
+nd = params[:n] / 26 + 1
 
-# entries per chunk
-# you might have to add one extra entry because the division isn't even
-epc = 1.upto(params[:n]).collect { |i| base_epc + (i <= extra_entries ? 1 : 0) }
+system "split -a #{nd} -l #{lpc} #{params[:fastq]} #{params[:fastq]}."
 
-# lines per chunk
-lpc = epc.map { |e| e * 4 }
-
-# what lines do you stop at?
-stops = lpc.inject([]) do |out, l|
-  if out.empty?
-    out << l
-  else
-    out << l + out.last
-  end
+# move the created files to where they are expected
+Dir.glob("#{params[:fastq]}.*").each_with_index do |fn, i|
+  FileUtils.move fn, fn.sub(/(?<=\.)[a-z]+$/, i.to_s)
 end
-stops.pop
-
-# what breaks do you feed to csplit?
-breaks = stops.map { |x| x+1 }.join(" ")
-
-exec "csplit -n #{nd} -f #{params[:fastq]}. #{params[:fastq]} #{breaks}"
