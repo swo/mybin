17478c99ade21049552741e546e853282e101897
swo <swo@mit.edu>
2015-02-11 09:26:51 -0500

    gcbp: fixed line count, pipe

--- get_columns_by_pattern.rb
+++ get_columns_by_pattern.rb
@@ -3,7 +3,6 @@
 # author:: scott w olesen (swo@mit.edu)
 
 require 'arginine'
-require 'set'
 
 params = Arginine::parse do 
   opt :separator, :short => "F", :default => "\t"
@@ -19,23 +18,23 @@ if params[:table]
   params[:include] = true
 end
 
-idx = []
-ARGF.lineno = 0
+fields = ARGF.gets.chomp.split(params[:separator])
+headers = fields.select { |f| f.match(params[:pattern]) }
+raise "no headers match pattern" if headers.empty?
+idx = headers.map { |header| fields.index(header) }
+
+if params[:one]
+  idx.unshift 0
+  headers.unshift fields.first
+end
+
+puts headers.join(params[:separator]) if params[:include]
+
 ARGF.each do |line|
-  fields = line.chomp.split(params[:separator])
-  if $. == 1
-    headers = fields.select { |f| f.match(params[:pattern]) }
-    raise "no headers match pattern" if headers.empty?
-    
-    idx = headers.map { |header| fields.index(header) }
-
-    if params[:one]
-      idx.unshift 0
-      headers.unshift fields.first
-    end
-
-    puts headers.join(params[:separator]) if params[:include]
-  else
+  begin
+    fields = line.chomp.split(params[:separator])
     puts fields.values_at(*idx).join(params[:separator])
+  rescue Errno::EPIPE
+    exit
   end
 end
