a2a3e7eaf4e84f7f72175598a028dcd2c60e8ba9
swo <swo@mit.edu>
2015-02-16 14:14:05 -0500

    drop lines: no $.

--- drop_lines.rb
+++ drop_lines.rb
@@ -6,8 +6,9 @@ require 'arginine'
 
 params = Arginine.parse do
   desc "drops first N lines from a file"
-  opt :drop, :short => "N", :default => 1, :cast => :to_i
+  opt :n, :default => 1, :cast => :to_i
   argf
 end
 
-ARGF.each { |line| puts line.chomp if $. > params[:drop] }
+1.upto(params[:n]).each { |i| ARGF.gets }
+ARGF.each_line { |line| puts line.chomp }
