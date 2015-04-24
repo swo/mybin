313c5aa66a62b5bf8d3ce57c07187b89f05837a7
swo <swo@mit.edu>
2015-04-24 10:00:01 -0400

    fastagrep uses inverse

--- fastagrep.rb
+++ fastagrep.rb
@@ -5,12 +5,19 @@
 require 'arginine'
 require 'bio'
 
-par = Arginine::parse do |a|
+$par = Arginine::parse do |a|
   desc "get the fasta entries that match a pattern"
   arg :pattern
+  flag :inverse, short: "v", desc: "get entries that don't match instead"
   argf "fasta"
 end
 
+def entry_match(entry)
+  found = entry.definition.gsub(/;.*/, '').match($par[:pattern])
+  found = (not found) if $par[:inverse]
+  return found
+end
+
 Bio::FlatFile.auto(ARGF).each_entry do |e|
-  puts e if e.definition.gsub(/;.*/, '').match(par[:pattern])
+  puts e if entry_match(e)
 end
