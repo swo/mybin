395e6033c6281ab565ff3b6d96eb552ea30ce8b3
swo <swo@mit.edu>
2015-05-21 11:02:09 -0400

    cleaned up & improved

--- pdf_drop_pages.py
+++ pdf_drop_pages.py
@@ -13,20 +13,25 @@ def number_of_pages(fn):
     if len(vals) != 1:
         raise RuntimeError("error parsing data dump")
 
-    return vals[0]
+    return int(vals[0])
 
-def drop_first_page(input_fn, output_fn):
+def drop_first_pages(input_fn, output_fn, n):
     # check how many pages are in the file
-    n = number_of_pages(input_fn)
-    pages_to_keep = "2-{}".format(n)
+    total_n = number_of_pages(input_fn)
+
+    if not n < total_n:
+        raise RuntimeError("can't drop {} pages from a pdf with only {} pages".format(n, n_total))
+
+    pages_to_keep = "{}-{}".format(n + 1, total_n)
 
     subprocess.call(['pdftk', input_fn, 'cat', pages_to_keep, 'output', output_fn])
 
 
 if __name__ == '__main__':
-    p = argparse.ArgumentParser(description='drop the first page of a pdf using pdftk')
+    p = argparse.ArgumentParser(description='drop the first page(s) of a pdf using pdftk', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('input')
     p.add_argument('output')
+    p.add_argument('-n', type=int, default=1, help='number of pages to drop')
     args = p.parse_args()
 
-    drop_first_page(args.input, args.output)
+    drop_first_pages(args.input, args.output, args.n)
