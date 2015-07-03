405a00e013544c6f0f427ce6db54852bedc04ddb
swo <swo@mit.edu>
2015-07-03 17:42:01 -0400

    topper: nicer

--- topper.py
+++ topper.py
@@ -6,24 +6,34 @@ import argparse, subprocess, time
 import curses
 
 class Timer:
+    def __init__(self, now_precision=1):
+        assert(0 < now_precision < 60)
+        self.now_precision = now_precision
+
     def reset(self):
         self.time = time.time()
 
     def elapsed(self):
         return time.time() - self.time
 
-    @staticmethod
-    def pretty_elapsed_seconds(seconds):
-        if seconds <= 1:
+    def pretty_elapsed_seconds(self, seconds):
+        if seconds < self.now_precision:
             return "now"
-        elif 1 < seconds < 60:
-            return "{} seconds ago".format(seconds)
+        elif seconds < 60:
+            return "{} {} ago".format(seconds, self.pluralize("second", seconds))
         elif 60 <= seconds < 3600:
             return "{} minutes, {} seconds ago".format(seconds / 60, seconds % 60)
 
     def pretty_elapsed(self):
         seconds = int(round(self.elapsed()))
         return self.pretty_elapsed_seconds(seconds)
+
+    @staticmethod
+    def pluralize(s, n):
+        if n == 1:
+            return s
+        else:
+            return s + "s"
 
 def current_display_time():
     return time.strftime('%H:%M:%S', time.localtime())
@@ -50,7 +60,7 @@ def key_loop(stdscr, sleep, command, show_time):
                 break
 
         if check:
-            output = subprocess.check_output(command, shell=True)
+            output = subprocess.check_output(command, shell=True, universal_newlines=True)
 
             # only get the lines that will fit on the screen
             output = "\n".join(output.split("\n")[-stdscr_y: -1])
@@ -66,7 +76,7 @@ def key_loop(stdscr, sleep, command, show_time):
             check = False
 
         if show_time:
-            out = display_time() + "\tlast update: " + status_watch.pretty_elapsed()
+            out = current_display_time() + "\tlast update: " + status_watch.pretty_elapsed()
             stdscr.addstr(stdscr_y - 1, 0, out)
 
         if refresh_watch.elapsed() > sleep:
@@ -84,6 +94,6 @@ if __name__ == '__main__':
     args = p.parse_args()
 
     def main(stdscr):
-        key_loop(stdscr, sleep=args.sleep, command=args.command, show_time=args.time)
+        key_loop(stdscr, sleep=args.sleep, command=args.command, show_time=args.no_time)
 
     curses.wrapper(main)
