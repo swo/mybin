cf0b689d658a54b18128239e504402699b9bf491
swo <swo@mit.edu>
2015-09-17 08:35:37 -0400

    some changes

--- fasta_grep.pl
+++ fasta_grep.pl
@@ -5,16 +5,21 @@ use strict;
 use 5.10.1;
 
 if (grep($_ eq "-h" || $_ eq "--help", @ARGV)) {
-  say "usage: fasta_grep.pl PATTERN [files]"; exit;
+  say "usage: fasta_grep.pl M PATTERN [files]"; exit;
 }
 
+my $max_hits = shift @ARGV;
 my $pattern = shift @ARGV;
+my $hits = 0;
 
 my $state = 0;
 while (<>) {
   if (/^>/) {
-    if (/$pattern/) {
+    if ($hits >= $max_hits) {
+      last;
+    } elsif (/$pattern/) {
       $state = 1;
+      $hits += 1;
       print;
     } else {
       $state = 0;
--- get_columns_by_pattern.rb
+++ get_columns_by_pattern.rb
@@ -4,36 +4,45 @@
 
 require 'arginine'
 
-params = Arginine::parse do 
+$params = Arginine::parse do 
   opt :separator, :short => "F", :default => "\t"
   arg :pattern
   flag :include, :desc => "include headers in output?"
   flag :one, :short => "1", :desc => "include first header?"
   flag :table, desc: "equivalent to -1 -i"
+  flag :inverse, short: 'v', desc: "invert match?"
   argf
 end
 
-if params[:table]
-  params[:one] = true
-  params[:include] = true
+if $params[:table]
+  $params[:one] = true
+  $params[:include] = true
 end
 
-fields = ARGF.gets.chomp.split(params[:separator])
-headers = fields.select { |f| f.match(params[:pattern]) }
-raise "no headers match pattern: i couldn't find /#{params[:pattern]}/ amongst #{fields}" if headers.empty?
+def my_match(field)
+  m = !field.match($params[:pattern]).nil?
+  v = $params[:inverse]
+  return ((m and not v) or (v and not m))
+end
+
+fields = ARGF.gets.chomp.split($params[:separator])
+headers = fields.select { |f| my_match(f) }
+raise "no headers match pattern: i couldn't find /#{$params[:pattern]}/ amongst #{fields}" if headers.empty?
 idx = headers.map { |header| fields.index(header) }
 
-if params[:one]
-  idx.unshift 0
-  headers.unshift fields.first
+if $params[:one]
+  if idx[0] != 0
+    idx.unshift 0
+    headers.unshift fields.first
+  end
 end
 
-puts headers.join(params[:separator]) if params[:include]
+puts headers.join($params[:separator]) if $params[:include]
 
 ARGF.each do |line|
   begin
-    fields = line.chomp.split(params[:separator])
-    puts fields.values_at(*idx).join(params[:separator])
+    fields = line.chomp.split($params[:separator])
+    puts fields.values_at(*idx).join($params[:separator])
   rescue Errno::EPIPE
     exit
   end
