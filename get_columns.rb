b8a4299d4cd64fe431474cf9e452b9c30a2e8b55
swo <swo@mit.edu>
2015-11-20 11:28:27 -0500

    get columns got smarter

--- get_columns.rb
+++ get_columns.rb
@@ -8,10 +8,19 @@ require 'set'
 params = Arginine::parse do
   desc "get columns by index"
   opt :separator, :short => "F", :default => "\t"
+  flag :whitespace, :short => "w", :help => "split on whitespace"
   arg :idx, :cast => lambda { |x| x.split(",").map(&:to_i) }, :desc => "indices of columns to be kept (comma-separated)"
   argf "lines to be searched"
 end
 
+if params[:whitespace]
+  input_separator = nil
+else
+  input_separator = params[:separator]
+end
+
+output_separator = params[:separator]
+
 ARGF.each do |line|
-  puts line.chomp.split(params[:separator]).values_at(*params[:idx]).join(params[:separator])
+  puts line.chomp.split(input_separator).values_at(*params[:idx]).join(output_separator)
 end
