1f92815a0f6aacbb068cf7048048d3e94d86f1f2
swo <swo@mit.edu>
2016-04-20 20:58:28 -0400

    pantex: made it smarter
    it will add a smart extension depending on the "to" type

--- pantex.py
+++ pantex.py
@@ -7,13 +7,25 @@ import argparse, os.path, os, sys
 if __name__ == '__main__':
     p = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     p.add_argument('input')
+    p.add_argument('--to', '-t', default='pdf', choices=['pdf', 'docx'])
+    p.add_argument('--verbose', '-v', action='store_true')
     args = p.parse_args()
 
     # check that this is a markdown file
     root, ext = os.path.splitext(args.input)
     assert ext in ['.md', '.markdown']
 
-    new_path = root + '.pdf'
+    # figure out pandoc "to" type based on the extension
+    if args.to == 'pdf':
+        pandoc_to = 'latex'
+    else:
+        pandoc_to = args.to
 
-    command = "pandoc --to latex -o {} {}".format(new_path, args.input)
+    new_path = root + '.' + args.to
+
+    command = "pandoc --to {} -o {} {}".format(pandoc_to, new_path, args.input)
+
+    if args.verbose:
+        print(command)
+
     os.system(command)
